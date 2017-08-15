ae69b10b730c9680160e703844e70bfc95d6447f
Matthew Hartley <mhartley@cantab.net>
2017-08-15 17:27:52 +0100

    Add store/get retrieve admin metadata

--- dtoolcore/storage_broker.py
+++ dtoolcore/storage_broker.py
@@ -1,6 +1,7 @@
 """Disk storage broker."""
 
 import os
+import json
 
 
 class StorageBrokerOSError(OSError):
@@ -13,14 +14,31 @@ class DiskStorageBroker(object):
 
     def __init__(self, path):
 
-        self._path = path
+        self._abspath = os.path.abspath(path)
+        self._dtool_abspath = os.path.join(self._abspath, '.dtool')
+        self._admin_metadata_fpath = os.path.join(self._dtool_abspath, 'dtool')
 
     def create_structure(self):
         """Create necessary structure to hold ProtoDataset or DataSet."""
 
-        if os.path.exists(self._path):
+        if os.path.exists(self._abspath):
             raise(StorageBrokerOSError(
-                "Path already exists: {}".format(self._path)
+                "Path already exists: {}".format(self._abspath)
             ))
 
-        os.mkdir(self._path)
+        os.mkdir(self._abspath)
+
+    def store_admin_metadata(self, admin_metadata):
+        """Store the admin metadata by writing to disk."""
+
+        if not os.path.isdir(self._dtool_abspath):
+            os.mkdir(self._dtool_abspath)
+
+        with open(self._admin_metadata_fpath, 'w') as fh:
+            json.dump(admin_metadata, fh)
+
+    def get_admin_metadata(self):
+        """Retrieve admin metadata from disk and return."""
+
+        with open(self._admin_metadata_fpath) as fh:
+            return json.load(fh)
--- tests/test_DiskStorageBroker.py
+++ tests/test_DiskStorageBroker.py
@@ -39,3 +39,20 @@ def test_create_structure(tmp_dir_fixture):  # NOQA
 
     with pytest.raises(OSError):
         storage_broker.create_structure()
+
+
+def test_store_and_retrieve_admin_metadata(tmp_dir_fixture):
+
+    from dtoolcore.storage_broker import DiskStorageBroker
+
+    destination_path = os.path.join(tmp_dir_fixture, 'my_proto_dataset')
+    storage_broker = DiskStorageBroker(destination_path)
+
+    storage_broker.create_structure()
+
+    admin_metadata = {'hello': 'world'}
+    storage_broker.store_admin_metadata(admin_metadata)
+
+    storage_broker_2 = DiskStorageBroker(destination_path)
+    retrieved_admin_metadata = storage_broker_2.get_admin_metadata()
+    assert retrieved_admin_metadata == admin_metadata
