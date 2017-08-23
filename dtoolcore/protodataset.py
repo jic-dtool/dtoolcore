68d67dd0a106005ce62f5da1877581546b097541
Tjelvar Olsson <tjelvar.olsson@jic.ac.uk>
2017-08-23 10:36:05 +0100

    Add docstrings to ProtoDataSet

--- dtoolcore/protodataset.py
+++ dtoolcore/protodataset.py
@@ -12,6 +12,9 @@ from dtoolcore import __version__
 
 
 class ProtoDataSet(object):
+    """
+    Class for building up a dataset.
+    """
 
     def __init__(self, name, admin_metadata=None):
 
@@ -32,6 +35,15 @@ class ProtoDataSet(object):
 
     @classmethod
     def create(cls, uri, name):
+        """
+        Return a :class:`dtoolcore.ProtoDataSet`.
+
+        :params uri: unique resource identifier defining where the dataset will
+                     be stored
+        :params name: dataset name
+        :returns: :class:`dtoolcore.ProtoDataSet`
+        """
+
         proto_dataset = cls(name)
 
         proto_dataset._storage_broker = DiskStorageBroker(uri)
@@ -47,6 +59,14 @@ class ProtoDataSet(object):
 
     @classmethod
     def from_uri(cls, uri):
+        """
+        Return an existing :class:`dtoolcore.ProtoDataSet` from a URI.
+
+        :params uri: unique resource identifier where the existing
+                     :class:`dtoolcore.ProtoDataSet` is stored
+        :returns: :class:`dtoolcore.ProtoDataSet`
+        """
+
         storage_broker = DiskStorageBroker(uri)
         admin_metadata = storage_broker.get_admin_metadata()
         if admin_metadata["type"] != "protodataset":
@@ -60,12 +80,12 @@ class ProtoDataSet(object):
 
     @property
     def uuid(self):
-        """Return the proto dataset's UUID."""
+        """Return the UUID of the dataset."""
         return self._admin_metadata["uuid"]
 
     @property
     def name(self):
-        """Return the proto dataset's name."""
+        """Return the name of the dataset."""
         return self._admin_metadata["name"]
 
     def _generate_manifest(self):
@@ -86,6 +106,13 @@ class ProtoDataSet(object):
         return manifest
 
     def put_item(self, fpath, relpath):
+        """
+        Put an item into the dataset.
+
+        :param fpath: path to the item on disk
+        :param relpath: relative path name given to the item in the dataset as
+                        a handle
+        """
 
         self._storage_broker.put_item(
             fpath,
@@ -93,16 +120,47 @@ class ProtoDataSet(object):
         )
 
     def put_readme(self, content):
+        """
+        Put content into the README of the dataset.
+
+        The client is responsible for ensuring that the content is valid YAML.
+
+        :param content: string to put into the README
+        """
 
         self._storage_broker.put_readme(content)
 
     def get_readme_content(self):
+        """
+        Return the content of the README describing the dataset.
+
+        :returns: content of README as a string
+        """
         return self._storage_broker.get_readme_content()
 
     def add_item_metadata(self, handle, key, value):
+        """
+        Add metadata to a specific item in the :class:`dtoolcore.ProtoDataSet`.
+
+        :param handle: handle representing the relative path of the item in the
+                       :class:`dtoolcore.ProtoDataSet`
+        :param key: metadata key
+        :param value: metadata value
+        """
+
         self._storage_broker.add_item_metadata(handle, key, value)
 
     def freeze(self):
+        """
+        Convert :class:`dtoolcore.ProtoDataSet` to :class:`dtoolcore.DataSet`.
+
+        1. Generate and persist the manifest
+        2. Create and persist overlays from any item metadata that has been
+           added
+        3. Change the type of the dataset from "protodataset" to "dataset"
+           and add a "frozen_at" time stamp to the administrative metadata
+        4. Clean up using the storage broker's post freeze hook
+        """
 
         manifest = self._generate_manifest()
         self._storage_broker.put_manifest(manifest)
