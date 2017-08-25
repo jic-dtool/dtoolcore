ef313e770c4aa4201f6478c150e6236a127d7a57
Tjelvar Olsson <tjelvar.olsson@jic.ac.uk>
2017-08-25 11:34:26 +0100

    Reformat setup.py

--- setup.py
+++ setup.py
@@ -5,15 +5,17 @@ version = "2.0.0"
 url = "https://github.com/JIC-CSB/dtoolcore"
 readme = open('README.rst').read()
 
-setup(name="dtoolcore",
-      packages=["dtoolcore"],
-      version=version,
-      description="Core API for managing (scientific) data",
-      long_description=readme,
-      include_package_data=True,
-      author="Tjelvar Olsson",
-      author_email="tjelvar.olsson@jic.ac.uk",
-      url=url,
-      download_url="{}/tarball/{}".format(url, version),
-      install_requires=[],
-      license="MIT")
+setup(
+    name="dtoolcore",
+    packages=["dtoolcore"],
+    version=version,
+    description="Core API for managing (scientific) data",
+    long_description=readme,
+    include_package_data=True,
+    author="Tjelvar Olsson",
+    author_email="tjelvar.olsson@jic.ac.uk",
+    url=url,
+    download_url="{}/tarball/{}".format(url, version),
+    install_requires=[],
+    license="MIT"
+)
