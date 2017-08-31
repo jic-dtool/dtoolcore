87b8225215adb4ba6179de98fce3d506bef05c20
Tjelvar Olsson <tjelvar.olsson@jic.ac.uk>
2017-08-31 14:41:36 +0100

    Update URL in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 version = "2.0.0"
-url = "https://github.com/JIC-CSB/dtoolcore"
+url = "https://github.com/jic-dtool/dtoolcore"
 readme = open('README.rst').read()
 
 setup(
