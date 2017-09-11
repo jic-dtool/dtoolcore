6ebfb1bac32145caa918afe0469132eacda45b8f
Tjelvar Olsson <tjelvar.olsson@jic.ac.uk>
2017-09-11 14:58:11 +0100

    Add diff_sizes method to compare module

--- dtoolcore/compare.py
+++ dtoolcore/compare.py
@@ -10,3 +10,24 @@ def diff_identifiers(a, b):
     a_ids = set(a.identifiers)
     b_ids = set(b.identifiers)
     return a_ids.difference(b_ids), b_ids.difference(a_ids)
+
+
+def diff_sizes(a, b):
+    """Return list of tuples (identifier, size in a, size in b).
+
+    Assumes list of identifiers in a and b are identical.
+
+    :param a: first :class:`dtoolcore.DataSet`
+    :param b: second :class:`dtoolcore.DataSet`
+    :returns: list of tuples for all items with different sizes
+              (identifier, size in a, size in b)
+    """
+    difference = []
+
+    for i in a.identifiers:
+        a_size = a.item_properties(i)["size_in_bytes"]
+        b_size = b.item_properties(i)["size_in_bytes"]
+        if a_size != b_size:
+            difference.append((i, a_size, b_size))
+
+    return difference
--- tests/test_compare_module.py
+++ tests/test_compare_module.py
@@ -15,7 +15,7 @@ def create_test_files(directory):
     return fpaths
 
 
-def test_compare_identifiers(tmp_dir_fixture):  # NOQA
+def test_diff_identifiers(tmp_dir_fixture):  # NOQA
 
     from dtoolcore import (
         DataSet,
@@ -53,3 +53,42 @@ def test_compare_identifiers(tmp_dir_fixture):  # NOQA
         set([generate_identifier("b.txt")])
     )
     assert diff_identifiers(ds_a, ds_b) == expected
+
+
+def test_diff_sizes(tmp_dir_fixture):  # NOQA
+
+    from dtoolcore import (
+        DataSet,
+        generate_admin_metadata,
+        generate_proto_dataset,
+    )
+    from dtoolcore.utils import generate_identifier
+    from dtoolcore.compare import diff_sizes
+
+    fpaths = create_test_files(tmp_dir_fixture)
+
+    proto_ds_a = generate_proto_dataset(
+        admin_metadata=generate_admin_metadata("test_compare_1"),
+        prefix=tmp_dir_fixture,
+        storage="file")
+    proto_ds_a.create()
+    proto_ds_a.put_item(fpaths["he"], "file.txt")
+    proto_ds_a.freeze()
+
+    proto_ds_b = generate_proto_dataset(
+        admin_metadata=generate_admin_metadata("test_compare_2"),
+        prefix=tmp_dir_fixture,
+        storage="file")
+    proto_ds_b.create()
+    proto_ds_b.put_item(fpaths["she"], "file.txt")
+    proto_ds_b.freeze()
+
+    ds_a = DataSet.from_uri(proto_ds_a.uri)
+    ds_b = DataSet.from_uri(proto_ds_b.uri)
+
+    assert diff_sizes(ds_a, ds_a) == []
+
+    expected = [
+        (generate_identifier("file.txt"), 2, 3 ),
+    ]
+    assert diff_sizes(ds_a, ds_b) == expected
