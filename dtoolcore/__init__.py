718a92709ba7885573f09b65074d06183c0e3620
Tjelvar Olsson <tjelvar.olsson@jic.ac.uk>
2017-10-23 11:04:07 +0100

    Update version number to 2.9.0

--- dtoolcore/__init__.py
+++ dtoolcore/__init__.py
@@ -16,7 +16,7 @@ except ImportError:
 import dtoolcore.utils
 
 
-__version__ = "2.8.3"
+__version__ = "2.9.0"
 
 
 def _generate_storage_broker_lookup():
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 
-version = "2.8.3"
+version = "2.9.0"
 url = "https://github.com/jic-dtool/dtoolcore"
 readme = open('README.rst').read()
 
