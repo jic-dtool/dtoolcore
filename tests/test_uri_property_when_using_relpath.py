42ef663d00c5ee870d1291bc92f0fa9c1fe3b888
Tjelvar Olsson <tjelvar.olsson@jic.ac.uk>
2017-12-03 13:25:37 +0000

    Add test for bug 2

---
+++ tests/test_uri_property_when_using_relpath.py
@@ -0,0 +1,36 @@
+import os
+
+from . import chdir_fixture  # NOQA
+from . import TEST_SAMPLE_DATA
+
+def test_uri_property_when_using_relpath(chdir_fixture):  # NOQA
+
+    from dtoolcore import ProtoDataSet, generate_admin_metadata
+    from dtoolcore import DataSet
+    from dtoolcore.utils import generate_identifier
+    from dtoolcore.storagebroker import DiskStorageBroker
+
+    name = "my_dataset"
+    admin_metadata = generate_admin_metadata(name)
+    dest_uri = DiskStorageBroker.generate_uri(
+        name=name,
+        uuid=admin_metadata["uuid"],
+        prefix=".")
+
+    sample_data_path = os.path.join(TEST_SAMPLE_DATA)
+    local_file_path = os.path.join(sample_data_path, 'tiny.png')
+
+    # Create a minimal dataset
+    proto_dataset = ProtoDataSet(
+        uri=dest_uri,
+        admin_metadata=admin_metadata,
+        config_path=None)
+    proto_dataset.create()
+    proto_dataset.put_item(local_file_path, 'tiny.png')
+
+    proto_dataset.freeze()
+
+
+    dataset = DataSet.from_uri("./my_dataset")
+    expected_uri = "file://" + os.path.abspath("my_dataset")
+    assert dataset.uri == expected_uri
