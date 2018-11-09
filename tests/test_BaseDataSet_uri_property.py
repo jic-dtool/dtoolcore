49570c1b7dd5c62495a01db07fe070c34db18383
Tjelvar Olsson <tjelvar.olsson@gmail.com>
2018-11-09 11:48:35 +0000

    Fix windows issue with test_uri_property

--- tests/test_BaseDataSet_uri_property.py
+++ tests/test_BaseDataSet_uri_property.py
@@ -1,17 +1,16 @@
 import os
 
-from . import tmp_dir_fixture  # NOQA
+from . import tmp_uri_fixture  # NOQA
 
 
-def test_uri_property(tmp_dir_fixture):  # NOQA
+def test_uri_property(tmp_uri_fixture):  # NOQA
 
     from dtoolcore import _BaseDataSet
 
     admin_metadata = {
-        "name": os.path.basename(tmp_dir_fixture),
+        "name": os.path.basename(tmp_uri_fixture),
         "uuid": "1234",
     }
-    base_ds = _BaseDataSet(tmp_dir_fixture, admin_metadata, None)
+    base_ds = _BaseDataSet(tmp_uri_fixture, admin_metadata, None)
 
-    expected_uri = "file://localhost{}".format(tmp_dir_fixture)
-    assert base_ds.uri == expected_uri
+    assert base_ds.uri == tmp_uri_fixture
