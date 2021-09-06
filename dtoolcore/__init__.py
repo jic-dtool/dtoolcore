55daea4860ae98250415ce09323518171c6c8d05
Johannes Laurin Hoermann <johannes.hoermann@imtek.uni-freiburg.de>
2021-09-06 13:47:50 +0200

    FIX: copy_resume picks up source dataset's 'frozen_at' timestamp

--- dtoolcore/__init__.py
+++ dtoolcore/__init__.py
@@ -315,6 +315,12 @@ def copy_resume(src_uri, dest_base_uri, config_path=None, progressbar=None):
     proto_dataset = ProtoDataSet.from_uri(dest_uri)
 
     _copy_content(dataset, proto_dataset, progressbar)
+
+    # jlh, 2021/09/06: Do we need conditional checks here?
+    # As I understand the mechanism, copying only works for frozen source datasets. When the transfer is interrupted,
+    # the partially copied dataset resides as an unfrozen proto dataset at the destination. A proto dataset never has
+    # the "frozen_at" timestamp, thus this property must be reassigned when resuming the transfer process.
+    proto_dataset._admin_metadata["frozen_at"] = dataset._admin_metadata["frozen_at"]
     proto_dataset.freeze(progressbar=progressbar)
 
     return proto_dataset.uri
